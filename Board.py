b0a5e741b5ed83cea5901a309005f5d8778da479
Zach Mills <ztmills@mines.edu>
2018-04-06 21:39:17 -0600

    Both teams created, lights good

--- Board.py
+++ Board.py
@@ -12,8 +12,8 @@ class Board(SampleBase):
     
     def __init__(self, *args, **kwargs):
         super(Board, self).__init__(*args, **kwargs)
-        self.teamR = Team(255, 255, 255)
-        self.teamL = Team(255, 255, 255)
+        self.teamR = Team(255, 0, 0)
+        self.teamL = Team(0, 0, 255)
         self.grid = []
         for row in range(0, 8):
             self.grid.append([None, None, None, None, None, None, None, None])	
@@ -34,7 +34,6 @@ class Board(SampleBase):
             offset_canvas = self.matrix.SwapOnVSync(offset_canvas)
 
     ### Member Functions ###
-
     def lightPieces(self, offset_canvas, team):
         r = 0
         c = 0
@@ -46,8 +45,10 @@ class Board(SampleBase):
                     # there is something here, check if right team
                     #if (self.grid[row][col].team 	
                     # light up cell!
-                    print(r, c, team.r, team.g, team.b)
-                    self.lightCell(offset_canvas, r, c, team.r, team.g, team.b)
+ #                   if (piece.team == self.teamR):
+ #                       self.lightCell(offset_canvas, r, c, self.teamR.r, self.teamR.g, self.teamR.b)
+                    #print(r, c, team.r, team.g, team.b)
+                    self.lightCell(offset_canvas, r, c, piece.team.r, piece.team.g, piece.team.b)
                 c = c + 1
             r = r + 1
 
@@ -65,8 +66,25 @@ class Board(SampleBase):
         self.grid[0][7] = Rook(0, 7, self.teamR)
         self.grid[0][1] = Knight(0, 1, self.teamR)
         self.grid[0][6] = Knight(0, 6, self.teamR)
-        self.grid[0][3] = King(0, 3, self.teamR) 
-        
+        self.grid[0][3] = Queen(0, 3, self.teamR) 
+        self.grid[0][4] = King(0, 4, self.teamR)
+
+        # TEAM R
+        # create pawns for teamR
+        for col in range(0, 8):
+            self.grid[6][col] = Pawn(6, col, self.teamL)	
+        # create bishop for teamR
+        self.grid[7][2] = Bishop(7, 2, self.teamL)
+        self.grid[7][5] = Bishop(7, 5, self.teamL)
+        # create rook for teamR
+        self.grid[7][0] = Rook(7, 0, self.teamL)
+        self.grid[7][7] = Rook(7, 7, self.teamL)
+        self.grid[7][1] = Knight(7, 1, self.teamL)
+        self.grid[7][6] = Knight(7, 6, self.teamL)
+        self.grid[7][3] = Queen(7, 3, self.teamL) 
+        self.grid[7][4] = King(7, 4, self.teamL)
+
+
 
     def createPlayers(self):
         nameR = input("Enter player 1 name: ")
