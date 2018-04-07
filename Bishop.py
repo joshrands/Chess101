36304b6f81df8dccad6bb01aa1e1aceffe321b1e
ztmills <ztmills@mymail.mines.edu>
2018-04-06 20:03:41 -0600

    Fixed bishop and added comments

--- Bishop.py
+++ Bishop.py
@@ -1,6 +1,8 @@
+#child class of Piece that represents a Bishop
 from Piece import Piece
 class Bishop(Piece):
     def calcTargets(self, checkerTown):
 
+#Overwrite default print with special bishop print
     def printPiece(self):
-        print("Pawn at " + self.row + ", " + self.col)
+        print("Bishop at " + self.row + ", " + self.col)
