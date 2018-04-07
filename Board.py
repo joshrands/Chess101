9bc337bdda53ae576fc06557a2a1cf1e1fbb4681
Zach Mills <ztmills@mines.edu>
2018-04-06 18:58:51 -0600

    Got GameManager calling process in Board

--- Board.py
+++ Board.py
@@ -1,20 +1,28 @@
-# Board class - controls hardware and holds pieces
-
+#!/usr/bin/env python
 from samplebase import SampleBase
 
 class Board(SampleBase):
-	
-	def __init__(self):
-		# initialize stuff
+    def __init__(self, *args, **kwargs):
+        super(Board, self).__init__(*args, **kwargs)
+    
+    def lightCell(self, canvas, x, y, r, g, b):
+        #offset_canvas = self.matrix.CreateFrameCanvas()
+        for i in range(0, 4):
+            for j in range(0, 4):
+                canvas.SetPixel(x*4 + i, y*4 + j, r, g, b)
 
-	def lightCell(self, x=0, y=0, r=255, g=255, b=255):
-		canvas = self.matrix.CreateFrameCanvas()	
-		for i in range(0, 4):
-			for j in range(0, 4):
-				canvas.SetPixel(x + i, y + j, r, g, b)	
-	
-	def turnOffCell(self, x='A', y='0'):
 
-board = Board()
-board.lightCell(0, 1, 255, 0, 0)
+    def run(self):
+        print("Running lights")
+        while True:
+            offset_canvas = self.matrix.CreateFrameCanvas()
+            
+            self.lightCell(offset_canvas, 4, 3, 64, 180, 232)
 
+            offset_canvas = self.matrix.SwapOnVSync(offset_canvas)
+
+# Main function
+#if __name__ == "__main__":
+#    simple_square = Board()
+#    if (not simple_square.process()):
+#        simple_square.print_help()
--- GameManager.py
+++ GameManager.py
@@ -1,12 +1,14 @@
 # GameManager class
 
+from Board import Board
+
 class GameManager:
 
-	
+    def __init__(self):
+        # make stuff
+        print("Hello world!")
 
-	def __init__(self):
-		# make stuff
+#    def setGameProperties():
 
-	def setGameProperties():
-
-
+board = Board()
+board.process()
