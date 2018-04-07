06da847610453512312e404bf11a85cfbbafc724
ztmills <ztmills@mymail.mines.edu>
2018-04-06 23:39:54 -0600

    fixing pawn still. Knight fixed?!

--- Knight.py
+++ Knight.py
@@ -4,7 +4,7 @@ from Cell import Cell
 
 class Knight(Piece):
 
-    def deathLoc(self, checkerTown, row, col):
+    def deathLoc(self, checkerTown, dir1, dir2, row, col):
         if (checkerTown[row + dir1][col + dir2] == None):
             self.targets.append(Cell(row + dir1, col + dir2))
         elif (checkerTown[row + dir1][col + dir2].team != self.team):
@@ -12,14 +12,14 @@ class Knight(Piece):
 
     def calcTargets(self, checkerTown):
         #check each of the knighty locations
-        self.deathLoc(checkerTown, 2, 1)
-        self.deathLoc(checkerTown, 2, -1)
-        self.deathLoc(checkerTown, -2, 1)
-        self.deathLoc(checkerTown, -2, -1)
-        self.deathLoc(checkerTown, 1, 2)
-        self.deathLoc(checkerTown, 1, -2)
-        self.deathLoc(checkerTown, -1, 2)
-        self.deathLoc(checkerTown, -1, -2)
+        self.deathLoc(checkerTown, 2, 1, self.row, self.col)
+        self.deathLoc(checkerTown, 2, -1, self.row, self.col)
+        self.deathLoc(checkerTown, -2, 1, self.row, self.col)
+        self.deathLoc(checkerTown, -2, -1, self.row, self.col)
+        self.deathLoc(checkerTown, 1, 2, self.row, self.col)
+        self.deathLoc(checkerTown, 1, -2, self.row, self.col)
+        self.deathLoc(checkerTown, -1, 2, self.row, self.col)
+        self.deathLoc(checkerTown, -1, -2, self.row, self.col)
 
 #Overwrite default print with special Knight print
     def printPiece(self):
