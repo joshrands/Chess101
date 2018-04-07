af805be5c9239cfc8b25787245fbcce157cac4d6
ztmills <ztmills@mymail.mines.edu>
2018-04-06 20:27:52 -0600

    else

--- Pawn.py
+++ Pawn.py
@@ -19,13 +19,21 @@ class Pawn(Piece):
             #should technically be a queen or something else now
         else:
             #check the squares diagonal in the direction of self.direction for an enemy Piece
+            #
             if (self.col != 0 and isinstance(Piece, checkerTown[self.row + self.direction][self.col - 1]):
                 if (checkerTown[self.row + self.direction][self.col - 1].team != self.team):
-                    targets.append()
-            #check the square in front
-            if (checkerTown[self.row + self.direction][self.column] != None and )
-            #if row and col = starting row and col then check 2 in front
+                    targets.append(Cell(self.row + self.direction, self.col - 1))
 
+            #Check the diagonal to the right
+            if (self.col != 7 and isinstance(Piece, checkerTown[self.row + self.direction][self.col + 1]):
+                if (checkerTown[self.row + self.direction][self.col - 1].team != self.team):
+                    targets.append(Cell(self.row + self.direction, self.col - 1))
+
+            #check the square in front
+            if (checkerTown[self.row + self.direction][self.column] != None):
+            #if row and col = starting row and col then check 2 in front
+            if (self.row == self.startingRow and self.col == self.startingCol):
+                if (checkerTown[self.row + 2 * self.direction][self.col] == None):
             #check for en passant
 
     #override move method to set enPassantable
