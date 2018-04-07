017937b375f6fd03ca82c970d1a3ab25cf4a7185
Zach Mills <ztmills@mines.edu>
2018-04-06 19:14:59 -0600

    yay

--- GameManager.py
+++ GameManager.py
@@ -5,12 +5,12 @@ from Board import Board
 class GameManager:
 
     def __init__(self):
-		# make stuff
-		print("Hello world!")
-		self.board = Board()
+        # make stuff
+        print("Hello world!")
+        self.board = Board()
 
-	def startGame(self):
-		self.board.process()
+    def startGame(self):
+        self.board.process()
 
 #    def setGameProperties():
 game = GameManager()
