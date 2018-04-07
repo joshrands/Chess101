1c0e40685eee548f25c4e34acf1289e1af182e19
ztmills <ztmills@mymail.mines.edu>
2018-04-06 21:26:20 -0600

    Rook functional

--- Rook.py
+++ Rook.py
@@ -2,6 +2,18 @@
 from Piece import Piece
 class Rook(Piece):
     def calcTargets(self, checkerTown):
+        #Run a recurssive function in all directions
+        bladeRunner(checkerTown, 1, 0, self.row, self.col)
+        bladeRunner(checkerTown, 0, 1, self.row, self.col)
+        bladeRunner(checkerTown, -1, 0, self.row, self.col)
+        bladeRunner(checkerTown, 0, -1, self.row, self.col)
+
+    def bladeRunner(self, checkerTown, dir1, dir2, row, col):
+        if (checkerTown[row + dir1][col + dir2] == None):
+            targets.append(Cell(row + dir1, col + dir2))
+            bladeRunner(checkerTown, dir1, dir2, row + dir1, col + dir2)
+        elif (checkerTown[row + dir1][col + dir2].team != self.team):
+            targets.append(Cell(row + dir1, col + dir2))
 
 #Overwrite default print with special Rook print
     def printPiece(self):
