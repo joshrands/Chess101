3e9c6ed24839a5aeca5ef2515771dc0335c2213d
ztmills <ztmills@mymail.mines.edu>
2018-04-07 13:51:11 -0600

    Now I've changed the colors

--- Board.py
+++ Board.py
@@ -12,14 +12,14 @@ from Cell import Cell
 import random
 
 class Board(SampleBase):
-    
+
     def __init__(self, *args, **kwargs):
         super(Board, self).__init__(*args, **kwargs)
-        self.teamR = Team(255, 0, 0)
-        self.teamL = Team(0, 0, 255)
+        self.teamR = Team(64, 180, 232)
+        self.teamL = Team(255, 140, 0)
         self.grid = []
         for row in range(0, 8):
-            self.grid.append([None, None, None, None, None, None, None, None])	
+            self.grid.append([None, None, None, None, None, None, None, None])
 
     # RUN GAME
     def run(self):
@@ -30,7 +30,7 @@ class Board(SampleBase):
 
         while True:
             offset_canvas = self.matrix.CreateFrameCanvas()
-            self.lightPieces(offset_canvas, self.teamR)			
+            self.lightPieces(offset_canvas, self.teamR)
             offset_canvas = self.matrix.SwapOnVSync(offset_canvas)
 
             # Do player 1's turn
@@ -39,7 +39,7 @@ class Board(SampleBase):
             offset_canvas = self.matrix.CreateFrameCanvas()
             self.doTurn(offset_canvas, self.teamL)
             offset_canvas = self.matrix.CreateFrameCanvas()
-            
+
             offset_canvas = self.matrix.SwapOnVSync(offset_canvas)
 
     ### Member Functions ###
@@ -73,12 +73,12 @@ class Board(SampleBase):
         check = False
         if (check):
             print("Check!")
-        
+
         move = False
         row = 0
         col = 0
-        print("Player:", team.name, "'s move.") 
-           
+        print("Player:", team.name, "'s move.")
+
         while (move == False):
             # move a piece!
             row = int(input("Enter row for desired piece: "))
@@ -92,7 +92,7 @@ class Board(SampleBase):
         self.lightPieces(canvas, self.teamR)
         self.lightTargets(canvas, self.grid[row][col])
         canvas = self.matrix.SwapOnVSync(canvas)
-    
+
         validMove = False
         # check if valid move
         while (validMove == False):
@@ -104,7 +104,7 @@ class Board(SampleBase):
                     print("Moving piece...")
                     self.grid[targetRow][targetCol] = self.grid[row][col]
                     if (isinstance(self.grid[targetRow][targetCol], Pawn)):
-                        enemy = self.grid[targetRow][targetCol].move(targetRow, targetCol) 
+                        enemy = self.grid[targetRow][targetCol].move(targetRow, targetCol)
                         if (enemy != None):
                             self.grid[enemy.row][enemy.col] = None
                             print("enPassant!")
@@ -114,15 +114,15 @@ class Board(SampleBase):
                         if (rookLocation != None):
                             # do castling
                             self.grid[rookTarget.row][rookTarget.col] = self.grid[rookLocation.row][rookLocation.col]
-                            self.grid[rookLocation.row][rookLocation.col] = None 
+                            self.grid[rookLocation.row][rookLocation.col] = None
                     else:
                         self.grid[targetRow][targetCol].move(targetRow, targetCol)
 
                     self.grid[row][col] = None
-            
+
             if (validMove == False):
                 print("Invalid target.")
-            else:   
+            else:
                 canvas = self.matrix.CreateFrameCanvas()
                 self.lightPieces(canvas, self.teamR)
                 canvas = self.matrix.SwapOnVSync(canvas)
@@ -142,13 +142,13 @@ class Board(SampleBase):
     def lightPieces(self, offset_canvas, team):
         r = 0
         c = 0
-        for row in self.grid: 
+        for row in self.grid:
             #print("Row:", r)
             c = 0
             for piece in row:
                 if (piece != None):
                     # there is something here, check if right team
-                    #if (self.grid[row][col].team 	
+                    #if (self.grid[row][col].team
                     # light up cell!
  #                   if (piece.team == self.teamR):
  #                       self.lightCell(offset_canvas, r, c, self.teamR.r, self.teamR.g, self.teamR.b)
@@ -162,7 +162,7 @@ class Board(SampleBase):
         # TEAM R
         # create pawns for teamR
         for col in range(0, 8):
-            self.grid[1][col] = Pawn(1, col, self.teamR)	
+            self.grid[1][col] = Pawn(1, col, self.teamR)
         # create bishop for teamR
         self.grid[0][2] = Bishop(0, 2, self.teamR)
         self.grid[0][5] = Bishop(0, 5, self.teamR)
@@ -171,13 +171,13 @@ class Board(SampleBase):
         self.grid[0][7] = Rook(0, 7, self.teamR)
         self.grid[0][1] = Knight(0, 1, self.teamR)
         self.grid[0][6] = Knight(0, 6, self.teamR)
-        self.grid[0][3] = Queen(0, 3, self.teamR) 
+        self.grid[0][3] = Queen(0, 3, self.teamR)
         self.grid[0][4] = King(0, 4, self.teamR)
 
         # TEAM R
         # create pawns for teamR
         for col in range(0, 8):
-            self.grid[6][col] = Pawn(6, col, self.teamL)	
+            self.grid[6][col] = Pawn(6, col, self.teamL)
         # create bishop for teamR
         self.grid[7][2] = Bishop(7, 2, self.teamL)
         self.grid[7][5] = Bishop(7, 5, self.teamL)
@@ -186,18 +186,18 @@ class Board(SampleBase):
         self.grid[7][7] = Rook(7, 7, self.teamL)
         self.grid[7][1] = Knight(7, 1, self.teamL)
         self.grid[7][6] = Knight(7, 6, self.teamL)
-        self.grid[7][3] = Queen(7, 3, self.teamL) 
+        self.grid[7][3] = Queen(7, 3, self.teamL)
         self.grid[7][4] = King(7, 4, self.teamL)
 
 
 
     def createPlayers(self):
         nameR = input("Enter player 1 name: ")
-        print("Enter player 1 colors (rgb): ")	
+        print("Enter player 1 colors (rgb): ")
         self.teamR.setName(nameR)
         #self.teamR.setColor()
 
-        nameL = input("Enter player 2 name: ")		
+        nameL = input("Enter player 2 name: ")
         print("Enter player 2 colors (rgb): ")
         self.teamL.setName(nameL)
         #self.teamL.setColor()
