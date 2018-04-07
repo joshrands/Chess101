426eea6c0a2455768807cb3f8f3114a21a9b4cd4
ztmills <ztmills@mymail.mines.edu>
2018-04-06 19:49:40 -0600

    Quick Push

--- Pawn.py
+++ Pawn.py
@@ -1,8 +1,8 @@
-//child class of Piece that represents a Pawn
+#child class of Piece that represents a Pawn
 import Piece.py
 class Pawn(Piece):
-    def __init__(self, row=0, col=0):
-        super().__init__(self, row, col)
+    def __init__(self, row=0, col=0, team):
+        super().__init__(self, row, col, team)
         self.startingRow = row
         self.startingCol = col
         if (self.row == 6):
@@ -10,11 +10,14 @@ class Pawn(Piece):
         else:
             self.direction = 1;
 
-    //calcTargets, which is abstract in the parent
-    def calcTargets(self):
-        //check the squares diagonal in the direction of self.direction for an enemy Piece
-        //check the square in front
-        //if row and col = starting row and col then check 2 in front
+    #calcTargets, which is abstract in the parent
+    def calcTargets(self, board):
+        #check the squares diagonal in the direction of self.direction for an enemy Piece
+        if (board[self.row + self.direction][])
+        #check the square in front
+        if (board[self.direction][self.column] != None and )
+        #if row and col = starting row and col then check 2 in front
+
 
     def printPiece(self):
         print("Pawn at " + self.row + ", " + self.col)
--- Piece.py
+++ Piece.py
@@ -2,10 +2,11 @@
 class Piece:
 	# team?
 
-	def __init__(self, row=0, col=0):
+	def __init__(self, row=0, col=0, team):
 		self.row = row
 		self.col = col
 		self.targets = []
+		self.team = team
 
 	# abstract method calcTargets
 	def calcTargets(self):
@@ -17,7 +18,7 @@ class Piece:
 		self.row = newRow
 		self.col = newCol
 
-	def printPiece(self):
+	def printPiece(self, board):
 		raise NotImplementedError()
 
 	def getTargets():
