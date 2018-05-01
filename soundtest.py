b8b764cc034b6ff4cd7f47c7b50eb55c2799f6fb
ztmills <ztmills@mymail.mines.edu>
2018-05-01 00:07:45 -0600

    change file

--- soundtest.py
+++ soundtest.py
@@ -4,5 +4,5 @@ import os
 print(os.getcwd())
 pygame.mixer.pre_init(44100, 16, 2, 4096) #frequency, size, channels, buffersize pygame.mixer.pre_init(44100, 16, 2, 4096) #frequency, size, channels, buffersize
 pygame.init()
-sound = pygame.mixer.Sound('/home/pi/Documents/Chess101/test.wav')
+sound = pygame.mixer.Sound('/home/pi/Documents/Chess101/beautiful_friendship.wav')
 sound.play()
