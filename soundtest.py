c5f55911ac5f065ecbedb03240fa51176062ea86
ztmills <ztmills@mymail.mines.edu>
2018-04-30 23:58:01 -0600

    fix

--- soundtest.py
+++ soundtest.py
@@ -1,5 +1,5 @@
 import pygame
- pygame.mixer.pre_init(44100, 16, 2, 4096) #frequency, size, channels, buffersize pygame.mixer.pre_init(44100, 16, 2, 4096) #frequency, size, channels, buffersize
+pygame.mixer.pre_init(44100, 16, 2, 4096) #frequency, size, channels, buffersize pygame.mixer.pre_init(44100, 16, 2, 4096) #frequency, size, channels, buffersize
 pygame.init()
 
 def playSound(filename):
