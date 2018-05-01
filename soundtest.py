0ab16642e5a81d066abec2e9b696e368e130d764
ztmills <ztmills@mymail.mines.edu>
2018-05-01 00:08:23 -0600

    wait a bit

--- soundtest.py
+++ soundtest.py
@@ -1,8 +1,11 @@
 import pygame
 import os
+import time
 
 print(os.getcwd())
 pygame.mixer.pre_init(44100, 16, 2, 4096) #frequency, size, channels, buffersize pygame.mixer.pre_init(44100, 16, 2, 4096) #frequency, size, channels, buffersize
 pygame.init()
 sound = pygame.mixer.Sound('/home/pi/Documents/Chess101/beautiful_friendship.wav')
 sound.play()
+time.sleep(2)
+sound.stop()
