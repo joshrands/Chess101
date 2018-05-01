2fb22a979be96da8fab73959105b636b85102a7d
ztmills <ztmills@mymail.mines.edu>
2018-05-01 00:03:28 -0600

    working directory

--- soundtest.py
+++ soundtest.py
@@ -1,4 +1,7 @@
 import pygame
+import os
+
+print(os.getcwd())
 pygame.mixer.pre_init(44100, 16, 2, 4096) #frequency, size, channels, buffersize pygame.mixer.pre_init(44100, 16, 2, 4096) #frequency, size, channels, buffersize
 pygame.init()
 sound = pygame.mixer.Sound('test.wav')
