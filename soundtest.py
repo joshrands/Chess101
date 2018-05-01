3c3d9840e8234a5be9d2a34967b5fb28401005af
ztmills <ztmills@mymail.mines.edu>
2018-05-01 00:22:29 -0600

    new ideas

--- soundtest.py
+++ soundtest.py
@@ -1,12 +1,4 @@
-import pygame
-import os
-import time
-
-print(os.getcwd())
-pygame.mixer.pre_init(44100, 16, 2, 4096) #frequency, size, channels, buffersize pygame.mixer.pre_init(44100, 16, 2, 4096) #frequency, size, channels, buffersize
-pygame.init()
-pygame.mixer.init()
-sound = pygame.mixer.Sound('/home/pi/Documents/Chess101/beautiful_friendship.wav')
-sound.play()
-time.sleep(60)
-sound.stop()
+import simpleaudio as sa
+wave_obj = sa.WaveObject.from_wave_file("beautiful_friendship.wav")
+play_obj = wave_obj.play()
+play_obj.wait_done()
