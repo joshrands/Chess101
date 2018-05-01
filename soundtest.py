8d4456dbcabb8a2fe1b518b95d5e3d238270bfc2
ztmills <ztmills@mymail.mines.edu>
2018-05-01 00:02:37 -0600

    siomplify

--- soundtest.py
+++ soundtest.py
@@ -1,9 +1,5 @@
 import pygame
 pygame.mixer.pre_init(44100, 16, 2, 4096) #frequency, size, channels, buffersize pygame.mixer.pre_init(44100, 16, 2, 4096) #frequency, size, channels, buffersize
 pygame.init()
-
-def playSound(filename):
-    sound = pygame.mixer.Sound(filename)
-    sound.play()
-
-playSound('./test.wav')
+sound = pygame.mixer.Sound('test.wav')
+sound.play()
