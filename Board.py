0f741f6022fb2483faf90b5d1ce9ddc1a99808da
ztmills <ztmills@mymail.mines.edu>
2018-05-15 16:39:19 -0600

    no deepcopy

--- Board.py
+++ Board.py
@@ -385,7 +385,8 @@ class Board(SampleBase):
             canvas = self.matrix.SwapOnVSync(canvas)
 
     def sethVictory(self, canvas, team):
-        tempMatrix = copy.deepcopy(self.matrix)
+        tempMatrix = RGBMatrix()
+        tempMatrix = self.matrix
         if (team == self.teamL):
             team = self.teamR
         else:
