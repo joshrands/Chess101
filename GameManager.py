c1a279e4867ec238803c334115f76b2a1c1102d8
ztmills <ztmills@mymail.mines.edu>
2018-05-17 11:03:56 -0600

    re initialize and gamemanager not class

--- GameManager.py
+++ GameManager.py
@@ -1,19 +1,7 @@
-# GameManager class
+# GameManager file
 
 from Board import Board
 
-class GameManager:
-
-    def __init__(self):
-        # make stuff
-        print("Hello world!")
-        self.board = Board()
-
-    def startGame(self):
-        self.board.process()
-
-#    def setGameProperties():
-
-game = GameManager()
 while True:
-    game.startGame()
+    self.board = Board()
+    self.board.process()
