f663d8c1e4dce55b7d368685650dfabc237d7cf9
Zach Mills <ztmills@mines.edu>
2023-10-28 19:12:07 -0600

    Logger chown

--- logger.sh
+++ logger.sh
@@ -3,5 +3,7 @@
 mv -f /home/pi/Documents/Chess101/output.log.2 /home/pi/Documents/Chess101/output.log.3
 mv -f /home/pi/Documents/Chess101/output.log.1 /home/pi/Documents/Chess101/output.log.2
 mv -f /home/pi/Documents/Chess101/output.log /home/pi/Documents/Chess101/output.log.1
+touch /home/pi/Documents/Chess101/output.log
+chown pi:pi /home/pi/Documents/Chess101/output.log
 
 echo "finished moving logs"
