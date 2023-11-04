b64fe1f9b25ebbfbaf2ae9b1ff2cf6e04f1013cf
Zach Mills <ztmills@mines.edu>
2023-11-04 16:28:48 -0600

    bash god

--- boot.sh
+++ boot.sh
@@ -3,5 +3,8 @@
 dir=$(dirname $0)
 cd $dir
 ./logger.sh
+if [[ -n "$1" && "$1" == "tail" ]]; then
+	echo "tailing error logs"
+	tail -n0 -f logs/error.log &
+fi
 sudo python3 -u GameManager.py 2>> logs/error.log | tee logs/output.log
-
