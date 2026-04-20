9e6b12895a3815c9f92139f22d4665df0263590d
Seth Asadi <sethjacob11@yahoo.com>
2026-04-20 13:24:20 -0700

    removed unneeded dir

--- rgbmatrix/setup.py
+++
@@ -1,9 +0,0 @@
-import os, sys
-from distutils.core import setup, Extension
-
-lib_dir = os.path.expanduser(os.environ.get("RGB_LIB_DIR", "~/rpi-rgb-led-matrix"))
-
-setup(ext_modules=[
-    Extension("core",     sources=["core.cpp"],     include_dirs=[f"{lib_dir}/include"], library_dirs=[f"{lib_dir}/lib"], libraries=["rgbmatrix"], extra_compile_args=["-std=c++11"], language="c++"),
-    Extension("graphics", sources=["graphics.cpp"], include_dirs=[f"{lib_dir}/include"], library_dirs=[f"{lib_dir}/lib"], libraries=["rgbmatrix"], extra_compile_args=["-std=c++11"], language="c++"),
-])
